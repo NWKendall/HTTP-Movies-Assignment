26f55cb804a1f64693fac3ed5ab4e07691a8675e
nicbongo <nicbongo@protonmail.com>
2020-01-30 11:45:18 -0400

    form populating

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,11 +2,14 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import MovieCard from "./MovieCard";
+
+
 export default class MovieList extends Component {
   constructor(props) {
+    console.log(`movieList props`, props)
     super(props);
     this.state = {
-      movies: []
+      movies: [],
     };
   }
 
@@ -17,11 +20,19 @@ export default class MovieList extends Component {
       .catch(err => console.log(err.response));
   }
 
+  handleUpdate = e => { // working
+    e.preventDefault();
+    this.props.history.push(`/update-form/${this.state.movie.id}`)
+  }
+
   render() {
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
+          <div>
+            <MovieDetails key={movie.id} movie={movie} />
+            <MovieBar key={movie.id} movie={movie}/>
+          </div>
         ))}
       </div>
     );
@@ -32,6 +43,16 @@ function MovieDetails({ movie }) {
   return (
     <Link to={`/movies/${movie.id}`}>
       <MovieCard movie={movie} />
+      
     </Link>
   );
+}
+
+function MovieBar({ movie }) {
+  return (
+    <div>
+      <Link to={`/update-form/${movie.id}`}>
+      </Link>
+    </div>
+  )
 }
--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,11 +1,86 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
-const UpdateMovie = () => {
+const initialFilm = {
+  id: "",
+  title: '',
+  director: '',
+  metascore: null,
+  stars: [],
+}
+
+const UpdateMovie = props => {
+  const [film, setFilm] = useState(initialFilm);
+  const { id } = useParams();
+
+  // useEffect(() => {
+  //   const movieToUpdate = props.movies.find(film => `${film.id}` === id);
+
+  //   if(movieToUpdate){
+  //     setFilm(movieToUpdate)
+  //   }
+  // }, [props.movies, id]);
+
+  
+  const fetchMovie = id => {
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then(res => this.setState({ movie: res.data }))
+      .catch(err => console.log(err.response));
+  };
+
+  const changeHandler = e => {
+    e.persist();    
+    setFilm({
+      ...film,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios 
+      .put(`http://localhost:5000/api/movies/${id}`, film)
+      .then(res => {
+        props.setMovies(res.data)
+      })
+      .catch(err => console.log(`FORM SUBMIT`, err))
+  }
+
   return (
     <div>
-      <button onClick={""}>Edit</button>
+      <h2>Update Movie</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          onChange={changeHandler}
+          value={film.title}
+        />
+        <input
+          type="text"
+          name="director"
+          onChange={changeHandler}
+          value={film.director}
+        />
+        <input
+          type="number"
+          name="metascore"
+          onChange={changeHandler}
+          value={film.metascore}
+        />
+        <input
+          type="text"
+          name="stars"
+          onChange={changeHandler}
+          value={film.stars}
+        />
+      </form>
     </div>
   )
 }
 
 export default UpdateMovie
+
+
